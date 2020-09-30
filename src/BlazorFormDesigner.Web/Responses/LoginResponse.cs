5743407c14f37d1eee2687b8527d947969704c48
Papp Kristóf Szabolcs <pappkristof3@gmail.com>
2020-09-30 13:27:27 +0200

    Login component, menu-bar

---
+++ src/BlazorFormDesigner.Web/Responses/LoginResponse.cs
@@ -0,0 +1,23 @@
+﻿using BlazorFormDesigner.Web.Models;
+using System;
+
+namespace BlazorFormDesigner.Web.Responses
+{
+    public class LoginResponse
+    {
+        public string Username { get; set; }
+
+        public string Name { get; set; }
+
+        public string Token { get; set; }
+
+        public string District { get; set; }
+
+        internal User ToUser()
+        {
+            var user = new User();
+            // TODO
+            return user;
+        }
+    }
+}
---
+++ src/BlazorFormDesigner.Web/Services/LoginService.cs
@@ -0,0 +1,54 @@
+﻿using BlazorFormDesigner.Web.Models;
+using BlazorFormDesigner.Web.Requests;
+using BlazorFormDesigner.Web.Responses;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace BlazorFormDesigner.Web.Services
+{
+    public class LoginService
+    {
+        public event Func<Task> LoginEvent;
+
+        private readonly HttpClient Client;
+
+        public User User { get; private set; }
+
+        public LoginService()
+        {
+            Client = new HttpClient { BaseAddress = new Uri("https://localhost:5001/") };
+        }
+
+        public async Task<ErrorResponse> Login(LoginRequest request)
+        {
+            try
+            {
+                var response = await Client.PutAsJsonAsync("login", request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var loginResponse = await response.Content.ReadAsAsync<LoginResponse>();
+                    User = loginResponse.ToUser();
+                    Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", User.Token);
+                    Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", User.Token);
+                    await LoginEvent?.Invoke();
+                    return null;
+                }
+
+                return await response.Content.ReadAsAsync<ErrorResponse>();
+            }
+            catch (Exception)
+            {
+                return new ErrorResponse { Content = "Unable to connect to server." };
+            }
+        }
+
+        public async Task Logout()
+        {
+            User = null;
+            await LoginEvent?.Invoke();
+        }
+    }
+}
