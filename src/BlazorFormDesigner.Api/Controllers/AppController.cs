35305e094c7e5c1c66c84bdfaf615761b786fc3b
Papp Kristóf Szabolcs <pappkristof3@gmail.com>
2020-10-06 10:41:33 +0200

    Implement UserController and UserService

---
+++ src/BlazorFormDesigner.Api/Controllers/AppController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BlazorFormDesigner.BusinessLogic.Models;
+using LoginApp.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorFormDesigner.Api.Controllers
+{
+    public class AppController : ControllerBase
+    {
+        protected readonly IMapper mapper;
+
+        protected AppController(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+
+        protected new User User
+        {
+            get
+            {
+                string token = Request.Headers["Authorization"];
+                return TokenService.DecodeToken(token?.Substring(7));
+            }
+        }
+    }
+}
---
+++ src/BlazorFormDesigner.Api/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using BlazorFormDesigner.BusinessLogic.Services;
+using BlazorFormDesigner.Web.Requests;
+using BlazorFormDesigner.Web.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorFormDesigner.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : AppController
+    {
+        private readonly UserService UserService;
+
+        public UserController(UserService userService, IMapper mapper) : base(mapper)
+        {
+            UserService = userService;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<LoginResponse>> Login(LoginRequest request)
+        {
+            var user = await UserService.ValidatePassword(request.Username, request.Password);
+
+            var token = TokenService.GenerateToken(user.Username, user.District, user.Type);
+
+            return Ok(user.ToDTO(mapper, token));
+        }
+    }
+}
